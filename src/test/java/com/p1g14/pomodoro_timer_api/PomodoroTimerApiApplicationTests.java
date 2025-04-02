7c87487960bcd818751d598822f9ef407972e5b1
naedher <166296413+naedher@users.noreply.github.com>
2025-04-02 11:05:17 +0200

    Initial commit

---
+++ src/test/java/com/p1g14/pomodoro_timer_api/PomodoroTimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p1g14.pomodoro_timer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroTimerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
