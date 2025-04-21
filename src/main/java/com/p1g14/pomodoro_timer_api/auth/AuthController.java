ae8aba8361e4190c489738eeca9b5ee28a83dc79
mucansu <mucansu@gmail.com>
2025-04-22 00:55:55 +0200

    getmapping changed to postmapping again, does not make any sense

--- src/main/java/com/p1g14/pomodoro_timer_api/auth/AuthController.java
+++ src/main/java/com/p1g14/pomodoro_timer_api/auth/AuthController.java
@@ -19,7 +19,7 @@ public class AuthController {
         return ResponseEntity.ok(authService.register(request));
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
