a2cf3661796e2439a482fe60416ac258c16f13f8
naedher <166296413+naedher@users.noreply.github.com>
2025-04-22 11:06:28 +0200

    Remove getAllTimers

--- src/main/java/com/p1g14/pomodoro_timer_api/timer/TimerService.java
+++ src/main/java/com/p1g14/pomodoro_timer_api/timer/TimerService.java
@@ -2,13 +2,11 @@ package com.p1g14.pomodoro_timer_api.timer;
 
 import com.p1g14.pomodoro_timer_api.timer.dto.TimerCreateRequest;
 import com.p1g14.pomodoro_timer_api.timer.dto.TimerDetailsResponse;
-import com.p1g14.pomodoro_timer_api.timer.dto.TimerListResponse;
 import com.p1g14.pomodoro_timer_api.timer.dto.TimerUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,15 +15,6 @@ public class TimerService {
     private final TimerRepository timerRepository;
 
     private TimerMapper timerMapper;
-
-    public List<TimerListResponse> getAllTimers() {
-
-        List<Timer> timers = timerRepository.findAll();
-
-        return timers.stream()
-                .map(timerMapper::toTimerListResponse)
-                .toList();
-    }
 
     public TimerDetailsResponse getTimerById(Long id) {
         Timer timer = timerRepository.getReferenceById(id);
@@ -43,7 +32,6 @@ public class TimerService {
         Timer timer = timerMapper.fromTimerCreateRequest(timerCreateRequest);
 
         timer.setCreatedAt(LocalDateTime.now());
-        timer.setUpdatedAt(LocalDateTime.now());
 
         Timer createdTimer = timerRepository.save(timer);
         return timerMapper.toTimerDetailsResponse(createdTimer);
