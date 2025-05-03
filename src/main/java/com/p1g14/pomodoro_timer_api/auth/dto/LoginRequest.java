ee0223f8bb645c02057aa29f649bdaaa4f7740a6
mucansu <mucansu@gmail.com>
2025-05-03 12:53:13 +0200

    Login and Register validation added

--- src/main/java/com/p1g14/pomodoro_timer_api/auth/dto/LoginRequest.java
+++ src/main/java/com/p1g14/pomodoro_timer_api/auth/dto/LoginRequest.java
@@ -1,9 +1,14 @@
 package com.p1g14.pomodoro_timer_api.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
+    @NotBlank(message = "Email can not be blank")
+    @Email(message = "Not a valid email adress")
     private String email;
+    @NotBlank(message = "Password can not be blank")
     private String password;
 }
--- src/main/java/com/p1g14/pomodoro_timer_api/auth/dto/RegisterRequest.java
+++ src/main/java/com/p1g14/pomodoro_timer_api/auth/dto/RegisterRequest.java
@@ -1,9 +1,15 @@
 package com.p1g14.pomodoro_timer_api.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class RegisterRequest {
+    @NotBlank
+    @Email(message = "Must be a valid email")
     private String email;
+    @NotBlank @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
