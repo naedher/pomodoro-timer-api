87b0674e63b3d04e1d4d9c203858f9f936a8e79d
naedher <166296413+naedher@users.noreply.github.com>
2025-05-09 15:47:35 +0200

    Use new exception in AuthService

--- src/main/java/com/p1g14/pomodoro_timer_api/auth/AuthService.java
+++ src/main/java/com/p1g14/pomodoro_timer_api/auth/AuthService.java
@@ -4,6 +4,7 @@ import com.p1g14.pomodoro_timer_api.auth.dto.LoginRequest;
 import com.p1g14.pomodoro_timer_api.auth.dto.AuthResponse;
 import com.p1g14.pomodoro_timer_api.auth.dto.RegisterRequest;
 import com.p1g14.pomodoro_timer_api.config.JwtService;
+import com.p1g14.pomodoro_timer_api.exception.EmailAlreadyExistsException;
 import com.p1g14.pomodoro_timer_api.user.User;
 import com.p1g14.pomodoro_timer_api.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +25,7 @@ public class AuthService {
 
     public AuthResponse register(RegisterRequest request) {
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new RuntimeException("Invalid credentials");
+            throw new EmailAlreadyExistsException("User with email already exists");
         }
 
         User user = new User();
